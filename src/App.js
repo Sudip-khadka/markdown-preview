78594cbf2812bbca58b0fdb155f1d421be5ae880
Sudip-khadka <ksudip0123@gmail.com>
2024-03-19 08:35:55 +0545

    dded texting env from freecodecamp.org

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World</h2>
+      <p className="paragraph">Sudip</p>
+    </div>
+  );
+}
+
+export default App;
